2de52a593c7922f399a12e112a27e09b5e87073b
Hussain Mostafa Al Haddad <hussain.business101@gmail.com>
2022-10-07 08:38:00 +0300

    refactor: create generic inboarding view controller
    This commit adds a geniric onboarding view controller that can be reused across multiple screens.

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -16,9 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-       // window?.rootViewController = OnboardingContainerViewController()
+        window?.rootViewController = OnboardingContainerViewController()
        // window?.rootViewController = LoginViewController()
-        window?.rootViewController = OnboardingViewController()
+      //  window?.rootViewController = OnboardingViewController(heroImageName: "delorean", titleText: "Bankey is faster, easiar to use, and has a brand new look and feel that will make you feel like you are back in 1989.")
         
         return true
     }
--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -14,10 +14,24 @@ class OnboardingViewController: UIViewController{
     let imageView = UIImageView()
     let label = UILabel()
     
+    let heroImageName: String
+    let titleText: String
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
+    }
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
 
@@ -27,21 +41,23 @@ extension OnboardingViewController{
     
     private func style(){
         
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
-       // stackView.backgroundColor = .systemPink
+        
         
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.textAlignment = .center
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easiar to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        label.text = titleText
     }
     
     private func layout(){
