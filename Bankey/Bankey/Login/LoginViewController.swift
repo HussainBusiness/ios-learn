f7ac6074c5070e467ab39d388502373f15b8c410
Hussain Mostafa Al Haddad <hussain.business101@gmail.com>
2022-10-16 11:29:42 +0300

    Feat: shake sign in button if login fails

--- Bankey/Bankey/Login/LoginViewController.swift
+++ Bankey/Bankey/Login/LoginViewController.swift
@@ -159,6 +159,18 @@ extension LoginViewController{
     private func configureView(withMessage message: String){
         errorMessageLabel.isHidden = false
         errorMessageLabel.text = message
+        shakeButton()
+    }
+    
+    private func shakeButton(){
+        let animation = CAKeyframeAnimation()
+        animation.keyPath = "position.x"
+        animation.values = [0, 10, -10, 10, 0]
+        animation.keyTimes = [0, 0.16, 0.5, 0.83, 1]
+        animation.duration = 0.4
+        
+        animation.isAdditive = true
+        signInButton.layer.add(animation, forKey: "shake")
     }
     
 }
@@ -183,6 +195,6 @@ extension LoginViewController{
         
         animator1.startAnimation()
         animator2.startAnimation(afterDelay: 1)
-        animator3.startAnimation(afterDelay: 0.4)
+        animator3.startAnimation(afterDelay: 0.2)
     }
 }
